12468e3b499c62efce8d489856357ca4744b5a2c
aks14y <akshay4362847@gmail.com>
2024-03-09 21:50:42 +0530

    feat: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,48 @@
+import DefaultLayout from "@/layout/DefaultLayout";
+import Image from "next/image";
+import notFound from "../assets/not_found.svg";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  const [counter, setCounter] = useState(10);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (counter > 0) {
+      setTimeout(() => {
+        setCounter(counter - 1);
+      }, 1000);
+    } else {
+      router.push("/");
+    }
+  }, [counter]);
+
+  return (
+    <DefaultLayout>
+      <div className="p-8 flex flex-wrap">
+        <div className="lg:w-2/4 md:px-36 md:py-32">
+          <div className="text-secondary text-center">
+            <h4 className="text-xl">
+              Oops! Looks like you're lost. Back Home in {counter}
+            </h4>
+            <Button
+              className="mt-4 p-6 text-lg text-black bg-primary"
+              as={Link}
+              href="/"
+            >
+              Take Me Home
+            </Button>
+          </div>
+        </div>
+        <Image
+          src={notFound}
+          alt="404"
+          className="lg:w-2/4 mt-16 h-1/2 lg:mt-0 w-auto "
+        />
+      </div>
+    </DefaultLayout>
+  );
 }
